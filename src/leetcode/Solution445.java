44ebfedec3e1c1bf1158d5da5c37951b425d7f32
wutianyang <1582869895@qq.com>
2020-09-02 18:15:54 +0800

    完成两数相加2

---
+++ src/leetcode/Solution445.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+import java.util.Stack;
+
+/**
+ * 两数相加 II
+ * 给你两个 非空 链表来代表两个非负整数。数字最高位位于链表开始位置。它们的每个节点只存储一位数字。将这两数相加会返回一个新的链表。
+ * <p>
+ * 你可以假设除了数字 0 之外，这两个数字都不会以零开头。
+ * <p>
+ * 输入：(7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 8 -> 0 -> 7
+ */
+public class Solution445 {
+
+    public class ListNode {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> stack1 = new Stack<>();
+        Stack<Integer> stack2 = new Stack<>();
+        while (l1 != null) {
+            stack1.push(l1.val);
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            stack2.push(l2.val);
+            l2 = l2.next;
+        }
+        ListNode result = null;
+        int carry = 0;
+        while (!stack1.isEmpty() || !stack2.isEmpty() || carry != 0) {
+            int num1 = stack1.isEmpty() ? 0 : stack1.pop();
+            int num2 = stack2.isEmpty() ? 0 : stack2.pop();
+            int sum = num1 + num2 + carry;
+            ListNode node = new ListNode(sum % 10);
+            carry = sum / 10;
+            node.next = result;
+            result = node;
+        }
+        return result;
+    }
+}
